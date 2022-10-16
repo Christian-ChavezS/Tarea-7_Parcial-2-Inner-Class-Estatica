2bddb1b8fac2114add904f7ac8a851d88f714db7
Christian-ChavezS <A330107@alumnos.uaslp.mx>
2022-10-15 19:52:34 -0500

    Tarea 7_Parcial 2 Inner Class Estatica. Alumno: Chavez Sanchez Christian José.

---
+++ src/uaslp/objetos/list/Linked_List/LinkedListIterator.java
@@ -0,0 +1,23 @@
+/* package uaslp.objetos.list.Linked_List;
+
+import uaslp.objetos.list.Iterator;
+
+private static class LinkedListIterator implements Iterator {
+    private Node current;
+
+
+    public LinkedListIterator(Node head){
+        this.current = head;
+    }
+
+    public boolean hasNext(){
+        return current != null;
+    }
+    public String next(){
+        String data = current.data;
+        current = current.next;
+        return data;
+    }
+}
+
+*/
